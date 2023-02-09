af9f5bef074ed6b14572b6366a803174c31d52ab
MusanjeLawrence <musanjelawrence6@gmail.com>
2023-02-09 07:37:13 +0300

    added serializable class

---
+++ serializable.java
@@ -0,0 +1,13 @@
+
+
+import java.io.Serializable;
+
+public class Faculty_SC implements Serializable {
+    String Faculty_ID,Faculty_Name;
+    
+    public Faculty_SC(){
+    Faculty_ID = "";
+    Faculty_Name ="";
+}
+    
+}
